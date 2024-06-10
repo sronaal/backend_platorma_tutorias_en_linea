8229083d83920c0f2e3e462729d41c3b887f237a
sronaalz <ronal.sanx21@gmail.com>
2024-06-09 20:02:29 -0500

    Configuracion inicial de rutas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require("express") // framework http para la creacion de controladores
 const cors = require("cors") // libreria para configurar el acceso al backend desde origines externos
 
+const {configuraciones_server} = require("./config/configuraciones")
+const rutasUsuarios = require("./app/routes/usuarios.routes")
+
 const app = express()
+
+
+// RUTAS DE LA APLICACIONES
+
+app.use(`${configuraciones_server.ruta}/${rutasUsuarios}`)
 
 
 app.use(cors({
