73c7bf02afad0325bc46f645fd4db24414837922
devYesid <ybernal317@gmail.com>
2024-06-24 16:47:51 -0500

    añadí configuracion de base de datos local para mí, corrección de variables

--- src/config/configuraciones.js
+++ src/config/configuraciones.js
@@ -1,33 +1,34 @@
-import path from 'path'
-import env from 'dotenv'
-import { fileURLToPath } from 'url';
+import path from "path";
+import env from "dotenv";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename);
 
-env.configDotenv(
-    {
-        path: path.resolve(__dirname,'.env')
-    }
-)
+env.configDotenv({
+  path: path.resolve(__dirname, ".env"),
+});
 
-console.log(__dirname)
+console.log(__dirname);
 
+export default {
+  port: 8080,
+  configuraciones_server: {
+    ruta: "/api/v1",
+    secret_key_dev: "DFA2z7j29k0X6HbH7b4TyBfc4FlFDsDd09GYP2VvuRE800m3Zw",
+  },
 
-export default  {
+  database: {
+    host: process.env.ipDB,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DB,
+  },
 
-    "port":8080,
-    "configuraciones_server":{
-
-        "ruta":"/api/v1",
-        "secret_key_dev": "DFA2z7j29k0X6HbH7b4TyBfc4FlFDsDd09GYP2VvuRE800m3Zw"
-    },
-
-    "database":{
-
-        "host": process.env.ipDB,
-        "user": process.env.USER,
-        "password": process.env.PASS,
-        "dabase": process.env.DB
-    }
-}
+  databaseYesid: {
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "tutorias",
+  },
+};
--- src/database_conexion.js
+++ src/database_conexion.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize"
 import config from './config/configuraciones.js'
 
 console.log(config)
-const database = new Sequelize(config.database.dabase,config.database.user,config.database.password,{
+const database = new Sequelize(config.database.database,config.database.user,config.database.password,{
 
     host: config.database.host,
     dialect:'mysql',
